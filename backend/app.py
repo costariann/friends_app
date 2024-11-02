5621f5eb08355eab5db8624d56b123295d39a18a
costariann <afuaoye30@gmail.com>
2024-11-02 23:00:18 +0000

    resolving cors issues

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,11 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "*"}}, supports_credentials=True)
+CORS(
+    app,
+    resources={r"/api/*": {"origins": "https://friends-app-6.onrender.com"}},
+    supports_credentials=True,
+)
 
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///friends.db"
